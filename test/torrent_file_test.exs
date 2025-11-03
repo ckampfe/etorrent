29af1202de3e75b25f268fc73882bb0a66b72965
Clark Kampfe <clark.kampfe@gmail.com>
2025-11-02 23:00:46 -0600

    rm commented out test

--- test/torrent_file_test.exs
+++ test/torrent_file_test.exs
@@ -245,21 +245,4 @@ defmodule Etorrent.TorrentFileTest do
             ]} =
              TorrentFile.piece_positions_lengths_and_hashes(info_hash)
   end
-
-  # test "roundtrips" do
-  #   f = File.read!("doomguy.png.torrent")
-  #   {:ok, decoded} = Bencode.decode(f, atom_keys: true)
-  #   {:ok, encoded} = Bencode.encode(decoded)
-  #   encoded_bin = :erlang.iolist_to_binary(encoded)
-
-  #   assert encoded_bin == f
-
-  #   info_hash =
-  #     decoded
-  #     |> Etorrent.info_hash()
-  #     |> Base.encode16(case: :lower)
-
-  #   # from Transmission 4.0.6
-  #   assert info_hash == "0b925b010556940a91ed5ed5a7f52f4ff209ec65"
-  # end
 end
